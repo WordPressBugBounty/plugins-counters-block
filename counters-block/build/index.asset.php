1532e9332d961f9b5a6dbd5b8cc0d490038ae813
Sean Murphy <sean@iamseanmurphy.com>
2024-10-03 15:57:14 +0000

    1.1.2

--- counters-block/build/index.asset.php
+++ counters-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'ba926ce6f2d53e99fdce');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '3026cf521cd7c83936c9');
